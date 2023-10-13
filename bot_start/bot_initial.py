6283316f42550c5bca4c9070763dd94dc0a8dd4c
Serhii Hrachov <bassdarkside@gmail.com>
2023-10-13 15:43:17 +0300

    upd bot_initial.py

--- bot_start/bot_initial.py
+++ bot_start/bot_initial.py
@@ -6,6 +6,7 @@ from decouple import config
 from schedule import every, repeat
 from telebot import types, custom_filters, util
 from bot_start.catalog import catalog, read_catalog, read_about, read_contacts
+from telebot.types import InputMediaPhoto
 from parser_v2.main import scrape_url, make_catalog
 
 TOKEN = config("TOKEN")
